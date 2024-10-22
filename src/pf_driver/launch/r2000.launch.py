e8845303b9b2be5166ad24f7d76b4a4863efe1a3
Berke Ünlü <berkeunlu@windowslive.com>
2024-10-22 14:40:25 +0300

    Update for SR's

--- src/pf_driver/launch/r2000.launch.py
+++ src/pf_driver/launch/r2000.launch.py
@@ -3,10 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
@@ -22,17 +19,11 @@ def generate_launch_description():
     node = Node(
         package='pf_driver',
         name='pf_r2000',
+        namespace=os.environ["SMR_PREFIX"],
         executable='ros_main',
         output='screen',
         parameters=[config]
     )
 
-    include_desc = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            FindPackageShare("pf_description"), '/launch', '/pf_bringup.launch.py']
-        )
-    )
-
     ld.add_action(node)
-    ld.add_action(include_desc)
     return ld
